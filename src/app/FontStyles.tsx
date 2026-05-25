e88c31895b2a99c00a6c4862dafde77121a0f1ba
2404mika <rakotomalalamika2@gmail.com>
2026-05-25 14:13:22 +0200

    Font changed

--- src/app/FontStyles.tsx
+++ src/app/FontStyles.tsx
@@ -4,7 +4,7 @@ export default function FontStyles() {
   return (
     <style jsx global>{`
       p, a, button, li, label, input, textarea, select {
-        font-family: 'Inter', sans-serif !important;
+        font-family: 'Elms Sans', sans-serif !important;
       }
       h1, h2, h3, h4, h5, h6, h1 span, h2 span, h3 span, h4 span, h5 span, h6 span {
         font-family: 'Zalando Sans Expanded', sans-serif !important;
